a086253258995e8634770069896e5e91bab4fcfe
Wolfgang Mattuttis <wolfgang@mattuttis.com>
2025-01-01 22:23:59 +0100

    Test coverage for Member component

--- src/app/member/member.component.spec.ts
+++ src/app/member/member.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MemberComponent } from './member.component';
+import {Member} from "../model/member";
 
 describe('MemberComponent', () => {
   let component: MemberComponent;
   let fixture: ComponentFixture<MemberComponent>;
+  let expectedMember: Member;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +16,20 @@ describe('MemberComponent', () => {
 
     fixture = TestBed.createComponent(MemberComponent);
     component = fixture.componentInstance;
+    expectedMember = {
+      id: '1',
+      first_name: 'John',
+      last_name: 'Doe'}
+    component.member = expectedMember;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should renter the title', () => {
+    const memberElement: HTMLElement = fixture.nativeElement;
+    expect(memberElement.querySelector('mat-card-title')?.textContent).toContain('John Doe');
   });
 });
